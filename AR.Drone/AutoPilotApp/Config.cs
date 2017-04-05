1d649f04697a3e818c5d2a7451fdc23ad206005c
Juan Manuel Servera <jmservera@outlook.com>
2017-04-05 19:19:17 +0200

    Add WPF autopilot

---
+++ AR.Drone/AutoPilotApp/Config.cs
@@ -0,0 +1,307 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AutoPilotApp
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>Occurs when a property value changes. </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>Updates the property and raises the changed event, but only if the new value does not equal the old value. </summary>
+        /// <param name="propertyName">The property name as lambda. </param>
+        /// <param name="oldValue">A reference to the backing field of the property. </param>
+        /// <param name="newValue">The new value. </param>
+        /// <returns>True if the property has changed. </returns>
+        public bool Set<T>(ref T oldValue, T newValue, [CallerMemberName] String propertyName = null)
+        {
+            return Set(propertyName, ref oldValue, newValue);
+        }
+
+        /// <summary>Updates the property and raises the changed event, but only if the new value does not equal the old value. </summary>
+        /// <param name="propertyName">The property name as lambda. </param>
+        /// <param name="oldValue">A reference to the backing field of the property. </param>
+        /// <param name="newValue">The new value. </param>
+        /// <returns>True if the property has changed. </returns>
+        public virtual bool Set<T>(String propertyName, ref T oldValue, T newValue)
+        {
+            if (Equals(oldValue, newValue))
+                return false;
+
+            oldValue = newValue;
+            RaisePropertyChanged(new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
+        /// <summary>Raises the property changed event. </summary>
+        /// <param name="propertyName">The property name. </param>
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            RaisePropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>Raises the property changed event. </summary>
+        /// <param name="args">The arguments. </param>
+        protected virtual void RaisePropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this,args);
+        }
+
+        /// <summary>Raises the property changed event for all properties (string.Empty). </summary>
+        public void RaiseAllPropertiesChanged()
+        {
+            RaisePropertyChanged(new PropertyChangedEventArgs(string.Empty));
+        }
+    }
+    public class ColorConfig : ObservableObject
+    {
+        public Color Low
+        {
+            get
+            {
+                return
+                ColorUtils.HsvToRgb(this.lowH * 2, this.lowS  / 255d, this.lowV  / 255d);
+            }
+        }
+
+        public Color High
+        {
+            get
+            {
+               return ColorUtils.HsvToRgb(this.highH * 2, this.highS / 255d, this.highV  / 255d);
+            }
+        }
+
+        int lowH;
+
+        public int LowH
+        {
+            get { return lowH; }
+            set { Set(ref lowH, value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(Low)));
+            }
+        }
+
+        private int highH;
+
+        public int HighH
+        {
+            get { return highH; }
+            set { Set(ref highH , value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(High)));
+            }
+        }
+
+        private int lowS;
+
+        public int LowS
+        {
+            get { return lowS; }
+            set { Set(ref lowS , value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(Low)));
+            }
+        }
+
+        private int highS;
+
+        public int HighS
+        {
+            get { return highS; }
+            set { Set(ref highS , value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(High)));
+            }
+        }
+
+        private int lowV;
+
+        public int LowV
+        {
+            get { return lowV; }
+            set { Set(ref lowV , value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(Low)));
+            }
+        }
+
+        private int highV;
+
+        public int HighV
+        {
+            get { return highV; }
+            set { Set(ref highV , value);
+            RaisePropertyChanged(new PropertyChangedEventArgs(nameof(High)));
+            }
+        }
+    }
+
+    public class Config:ObservableObject
+    {
+        private ColorConfig redConfig=new ColorConfig();
+
+        public ColorConfig RedConfig
+        {
+            get { return redConfig; }
+            set { Set(ref redConfig , value); }
+        }
+
+        private ColorConfig greenConfig=new ColorConfig();
+
+        public ColorConfig GreenConfig
+        {
+            get { return greenConfig; }
+            set { Set(ref greenConfig , value); }
+        }
+    }
+
+    public class Bitmaps: ObservableObject
+    {
+        private BitmapImage original;
+
+        public BitmapImage Original
+        {
+            get { return original; }
+            set { Set(ref original , value); }
+        }
+
+        private BitmapImage first;
+
+        public BitmapImage First
+        {
+            get { return first; }
+            set { Set(ref first , value); }
+        }
+
+        private BitmapImage second;
+
+        public BitmapImage Second
+        {
+            get { return second; }
+            set { Set(ref second , value); }
+        }
+
+        private BitmapImage final;
+
+        public BitmapImage Final
+        {
+            get { return final; }
+            set { Set(ref final , value); }
+        }
+    }
+
+    public static class BitmapExtensions
+    {
+        ///// <summary>
+        ///// Converts a <see cref="System.Drawing.Image"/> into a WPF <see cref="BitmapSource"/>.
+        ///// </summary>
+        ///// <param name="source">The source image.</param>
+        ///// <returns>A BitmapSource</returns>
+        //public static BitmapSource ToBitmapSource(this System.Drawing.Image source)
+        //{
+        //    System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(source);
+
+        //    var bitSrc = bitmap.ToBitmapSource();
+
+        //    bitmap.Dispose();
+        //    bitmap = null;
+
+        //    return bitSrc;
+        //}
+
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Bitmap"/> into a WPF <see cref="BitmapSource"/>.
+        /// </summary>
+        /// <remarks>Uses GDI to do the conversion. Hence the call to the marshalled DeleteObject.
+        /// </remarks>
+        /// <param name="source">The source bitmap.</param>
+        /// <returns>A BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(this System.Drawing.Bitmap source)
+        {
+            BitmapSource bitSrc = null;
+
+            var hBitmap = source.GetHbitmap();
+
+            try
+            {
+                bitSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    new Int32Rect(0,0,source.Width,source.Height),
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch (Win32Exception)
+            {
+                bitSrc = null;
+            }
+            finally
+            {
+                NativeMethods.DeleteObject(hBitmap);
+            }
+
+            return bitSrc;
+        }
+
+        /// <summary>
+        /// FxCop requires all Marshalled functions to be in a class called NativeMethods.
+        /// </summary>
+        internal static class NativeMethods
+        {
+            [DllImport("gdi32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool DeleteObject(IntPtr hObject);
+        }
+    }
+
+    public class ColorUtils
+    {
+        public static Color HsvToRgb(double h, double s, double v)
+        {
+            int hi = (int)Math.Floor(h / 60.0) % 6;
+            double f = (h / 60.0) - Math.Floor(h / 60.0);
+
+            double p = v * (1.0 - s);
+            double q = v * (1.0 - (f * s));
+            double t = v * (1.0 - ((1.0 - f) * s));
+
+            Color ret;
+
+            switch (hi)
+            {
+                case 0:
+                    ret = ColorUtils.GetRgb(v, t, p);
+                    break;
+                case 1:
+                    ret = ColorUtils.GetRgb(q, v, p);
+                    break;
+                case 2:
+                    ret = ColorUtils.GetRgb(p, v, t);
+                    break;
+                case 3:
+                    ret = ColorUtils.GetRgb(p, q, v);
+                    break;
+                case 4:
+                    ret = ColorUtils.GetRgb(t, p, v);
+                    break;
+                case 5:
+                    ret = ColorUtils.GetRgb(v, p, q);
+                    break;
+                default:
+                    ret = Color.FromArgb(0xFF, 0x00, 0x00, 0x00);
+                    break;
+            }
+            return ret;
+        }
+        public static Color GetRgb(double r, double g, double b)
+        {
+            return Color.FromArgb(255, (byte)(r * 255.0), (byte)(g * 255.0), (byte)(b * 255.0));
+        }
+    }
+}
---
+++ AR.Drone/AutoPilotApp/MainWindow.xaml.cs
@@ -0,0 +1,235 @@
+﻿using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+
+namespace AutoPilotApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Config config;
+        Bitmaps bitmaps;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var configObj = Application.Current.Resources["Config"];
+            config = configObj as Config;
+            if (config != null)
+            {
+                config.RedConfig.LowH = 141;
+                config.RedConfig.HighH = 255;
+                config.RedConfig.LowS = 56;
+                config.RedConfig.HighS = 130;
+                config.RedConfig.LowV = 145;
+                config.RedConfig.HighV = 255;
+            }
+            configObj = Application.Current.Resources["Bitmaps"];
+            bitmaps = configObj as Bitmaps;
+            this.DataContextChanged += (o, e) => {
+                System.Diagnostics.Debug.WriteLine(e.Property.Name);
+            };
+            Closed += MainWindow_Closed;
+        }
+
+        private async void MainWindow_Closed(object sender, EventArgs e)
+        {
+            await closeSource();
+        }
+
+        VideoCaptureDevice videoSource;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await closeSource();
+            //List all available video sources. (That can be webcams as well as tv cards, etc)
+            FilterInfoCollection videosources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            //Check if atleast one video source is available
+            if (videosources != null)
+            {
+                //For example use first video device. You may check if this is your webcam.
+                videoSource = new VideoCaptureDevice(videosources[0].MonikerString);
+
+                try
+                {
+                    //Check if the video device provides a list of supported resolutions
+                    if (videoSource.VideoCapabilities.Length > 0)
+                    {
+                        string highestSolution = "0;0";
+                        //Search for the highest resolution
+                        for (int i = 0; i < videoSource.VideoCapabilities.Length; i++)
+                        {
+                            if (videoSource.VideoCapabilities[i].FrameSize.Width > Convert.ToInt32(highestSolution.Split(';')[0]))
+                                highestSolution = videoSource.VideoCapabilities[i].FrameSize.Width.ToString() + ";" + i.ToString();
+                        }
+                        //Set the highest resolution as active
+                        videoSource.VideoResolution = videoSource.VideoCapabilities[Convert.ToInt32(highestSolution.Split(';')[1])];
+                    }
+                }
+                catch { }
+
+                //Create NewFrame event handler
+                //(This one triggers every time a new frame/image is captured
+                videoSource.NewFrame += new AForge.Video.NewFrameEventHandler(videoSource_NewFrame);
+
+                //Start recording
+                videoSource.Start();
+            }
+        }
+
+        void videoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            var action = new Action(() => analyze((Bitmap)eventArgs.Frame.Clone()));
+            //Cast the frame as Bitmap object and don't forget to use ".Clone()" otherwise
+            //you'll probably get access violation exceptions
+            if (!Dispatcher.CheckAccess()) // CheckAccess returns true if you're on the dispatcher thread
+            {
+                Dispatcher.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        void setPic(Bitmap bitmap, Action<BitmapImage> setter)
+        {
+            try
+            {
+                BitmapImage bi;
+                
+                    bi = new BitmapImage();
+                    bi.BeginInit();
+                    MemoryStream ms = new MemoryStream();
+                    bitmap.Save(ms, ImageFormat.Bmp);
+                    bi.StreamSource = ms;
+                    bi.CacheOption = BitmapCacheOption.OnLoad;
+                    bi.EndInit();
+                bi.Freeze();
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { setter(bi); }));
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch your error here
+            }
+
+        }
+
+        private async Task closeSource()
+        {
+            if (videoSource != null)
+            {
+                if (videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                }
+                while (videoSource.IsRunning)
+                    await Task.Delay(100);
+                videoSource = null;
+            }
+        }
+
+        private void analyze(System.Drawing.Bitmap bitmap)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(bitmap);
+
+            UMat uimage = new UMat();
+
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Hsv);
+
+
+            double cannyThreshold = 180.0;
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(img, cannyEdges, cannyThreshold, cannyThresholdLinking);
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+
+            foreach (LineSegment2D line in lines)
+                img.Draw(line, new Bgr(System.Drawing.Color.Green), 2);
+
+
+            //use image pyr to remove noise
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+
+            var currentConfig = config.RedConfig;
+            UMat imgThresholded = new UMat();
+            MCvScalar lower = new MCvScalar(currentConfig.LowH, currentConfig.LowS, currentConfig.LowV);
+            MCvScalar upper = new MCvScalar(currentConfig.HighH, currentConfig.HighS, currentConfig.HighV);
+
+            CvInvoke.InRange(uimage, new ScalarArray(lower), new ScalarArray(upper), imgThresholded);
+
+            //Calculate the moments of the thresholded image
+            var oMoments = CvInvoke.Moments(imgThresholded);
+
+            double dM01 = oMoments.M01;
+            double dM10 = oMoments.M10;
+            double dArea = oMoments.M00;
+
+            // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+            if (dArea > 10000)
+            {
+                //calculate the position of the ball
+                int posX = (int)(dM10 / dArea);
+                int posY = (int)(dM01 / dArea);
+
+                var blank = img.CopyBlank();
+                CvInvoke.Circle(img, new System.Drawing.Point(posX, posY), 20, new MCvScalar(255, 0, 0), -1);
+                //img = img + blank;
+            }
+
+            //if (bitmaps.Original == null)
+            //{
+            //    PixelFormat pf = PixelFormats.Rgb24;
+            //    int width = bitmap.Width;
+            //    int height = bitmap.Height;
+            //    int rawStride = (width * pf.BitsPerPixel + 7) / 8;
+            //    byte[] rawImage = new byte[rawStride * height];
+            //    bitmaps.Original = BitmapSource.Create(width, height, 96, 96, PixelFormats.Rgb24, null, bitmap., rawStride);
+            //    bitmaps.First= BitmapSource.Create(width, height, 96, 96, PixelFormats.Rgb24, null, rawImage, rawStride);
+            //    bitmaps.Second= BitmapSource.Create(width, height, 96, 96, PixelFormats.Rgb24, null, rawImage, rawStride);
+            //    bitmaps.Final= BitmapSource.Create(width, height, 96, 96, PixelFormats.Rgb24, null, rawImage, rawStride);
+            //}
+
+            setPic(bitmap, (o) => bitmaps.Original = o);
+            setPic(img.ToBitmap(), (o) => bitmaps.First = o);
+            setPic(uimage.Bitmap, (o) => bitmaps.Second = o);
+            setPic(imgThresholded.Bitmap, (o) => bitmaps.Final = o);
+            //bitmaps.Original = bitmap.ToBitmapSource();
+            //bitmaps.First= img.ToBitmap().ToBitmapSource();
+            //bitmaps.Second = uimage.Bitmap.ToBitmapSource();
+            //bitmaps.Final = imgThresholded.Bitmap.ToBitmapSource();
+        }
+    }
+}
