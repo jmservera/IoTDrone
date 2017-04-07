48746088b0466f74554d84221b4825073f17144d
Juan Manuel Servera <jmservera@outlook.com>
2017-04-07 20:26:11 +0200

    split analyzer to a class

---
+++ AR.Drone/AutoPilotApp/CV/Analyzer.cs
@@ -0,0 +1,84 @@
+﻿using AutoPilotApp.Models;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPilotApp.CV
+{
+    public class Analyzer
+    {
+        Bitmaps bitmaps;
+        public Analyzer(Bitmaps bitmaps)
+        {
+            this.bitmaps = bitmaps;
+        }
+
+        public void Analyze(System.Drawing.Bitmap bitmap, ColorConfig currentConfig)
+        {
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            bitmaps.UpdateImages(bitmap);
+            using (Image<Bgr, byte> img = new Image<Bgr, byte>(bitmaps.Bitmap))
+            {
+                UMat uimage = new UMat();
+                CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Hsv);
+
+                double cannyThreshold = 180.0;
+                double cannyThresholdLinking = 120.0;
+                UMat cannyEdges = new UMat();
+                CvInvoke.Canny(img, cannyEdges, cannyThreshold, cannyThresholdLinking);
+                LineSegment2D[] lines = CvInvoke.HoughLinesP(
+                   cannyEdges,
+                   1, //Distance resolution in pixel-related units
+                   Math.PI / 45.0, //Angle resolution measured in radians.
+                   20, //threshold
+                   30, //min Line width
+                   10); //gap between lines
+
+                foreach (LineSegment2D line in lines)
+                    img.Draw(line, new Bgr(System.Drawing.Color.Green), 2);
+
+                //use image pyr to remove noise
+                UMat pyrDown = new UMat();
+                CvInvoke.PyrDown(uimage, pyrDown);
+                CvInvoke.PyrUp(pyrDown, uimage);
+
+                UMat imgThresholded = new UMat();
+                MCvScalar lower = new MCvScalar(currentConfig.LowH, currentConfig.LowS, currentConfig.LowV);
+                MCvScalar upper = new MCvScalar(currentConfig.HighH, currentConfig.HighS, currentConfig.HighV);
+
+                CvInvoke.InRange(uimage, new ScalarArray(lower), new ScalarArray(upper), imgThresholded);
+
+                //Calculate the moments of the thresholded image
+                var oMoments = CvInvoke.Moments(imgThresholded);
+
+                double dM01 = oMoments.M01;
+                double dM10 = oMoments.M10;
+                double dArea = oMoments.M00;
+
+                // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+                if (dArea > 10000)
+                {
+                    //calculate the position of the ball
+                    int posX = (int)(dM10 / dArea);
+                    int posY = (int)(dM01 / dArea);
+                    CvInvoke.Circle(img, new System.Drawing.Point(posX, posY), (int)(dArea / 100000d),
+                        new MCvScalar(255, 0, 0), -1);
+                }
+
+                bitmaps.Calculations = sw.ElapsedMilliseconds;
+                sw.Restart();
+                bitmaps.UpdateImages(null, img.ToBitmap(),
+                    cannyEdges?.Bitmap,
+                    imgThresholded?.Bitmap);
+
+                bitmaps.ImageSet = sw.ElapsedMilliseconds;
+                bitmaps.FPS = bitmaps.Calculations + bitmaps.ImageSet;
+            }
+        }
+    }
+}
--- AR.Drone/AutoPilotApp/MainWindow.xaml.cs
+++ AR.Drone/AutoPilotApp/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ using System.Windows.Threading;
 using Emgu.CV.Util;
 using AutoPilotApp.Models;
 using System.ComponentModel;
+using AutoPilotApp.CV;
 
 namespace AutoPilotApp
 {
@@ -55,6 +56,7 @@ namespace AutoPilotApp
         }
 
         Bitmaps bitmaps;
+        Analyzer analyzer;
         DroneClient _droneClient;
         VideoPacketDecoderWorker _videoPacketDecoderWorker;
 
@@ -77,6 +79,7 @@ namespace AutoPilotApp
 
             var bmpsObj = Application.Current.Resources["Bitmaps"];
             bitmaps = bmpsObj as Bitmaps;
+            analyzer = new Analyzer(bitmaps);
 
             this.DataContextChanged += (o, e) => {
                 System.Diagnostics.Debug.WriteLine(e.Property.Name);
@@ -84,7 +87,6 @@ namespace AutoPilotApp
             Closed += MainWindow_Closed;
 
             configDrone();
-
             frameTimer = new DispatcherTimer(TimeSpan.FromMilliseconds(20), DispatcherPriority.Normal, timerElapsed, this.Dispatcher);
         }
 
@@ -223,64 +225,7 @@ namespace AutoPilotApp
 
         private void analyze(System.Drawing.Bitmap bitmap)
         {
-            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-            bitmaps.UpdateImages(bitmap);
-            using (Image<Bgr, byte> img = new Image<Bgr, byte>(bitmaps.Bitmap))
-            {
-                UMat uimage = new UMat();
-                CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Hsv);
-
-                double cannyThreshold = 180.0;
-                double cannyThresholdLinking = 120.0;
-                UMat cannyEdges = new UMat();
-                CvInvoke.Canny(img, cannyEdges, cannyThreshold, cannyThresholdLinking);
-                LineSegment2D[] lines = CvInvoke.HoughLinesP(
-                   cannyEdges,
-                   1, //Distance resolution in pixel-related units
-                   Math.PI / 45.0, //Angle resolution measured in radians.
-                   20, //threshold
-                   30, //min Line width
-                   10); //gap between lines
-
-                foreach (LineSegment2D line in lines)
-                    img.Draw(line, new Bgr(System.Drawing.Color.Green), 2);
-
-                //use image pyr to remove noise
-                UMat pyrDown = new UMat();
-                CvInvoke.PyrDown(uimage, pyrDown);
-                CvInvoke.PyrUp(pyrDown, uimage);
-
-                UMat imgThresholded = new UMat();
-                MCvScalar lower = new MCvScalar(currentConfig.LowH, currentConfig.LowS, currentConfig.LowV);
-                MCvScalar upper = new MCvScalar(currentConfig.HighH, currentConfig.HighS, currentConfig.HighV);
-
-                CvInvoke.InRange(uimage, new ScalarArray(lower), new ScalarArray(upper), imgThresholded);
-
-                //Calculate the moments of the thresholded image
-                var oMoments = CvInvoke.Moments(imgThresholded);
-
-                double dM01 = oMoments.M01;
-                double dM10 = oMoments.M10;
-                double dArea = oMoments.M00;
-
-                // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
-                if (dArea > 10000)
-                {
-                    //calculate the position of the ball
-                    int posX = (int)(dM10 / dArea);
-                    int posY = (int)(dM01 / dArea);
-                    CvInvoke.Circle(img, new System.Drawing.Point(posX, posY), (int)(dArea / 100000d), new MCvScalar(255, 0, 0), -1);
-                }
-
-                bitmaps.Calculations = sw.ElapsedMilliseconds;
-                sw.Restart();
-                bitmaps.UpdateImages(null,img.ToBitmap(),
-                    cannyEdges.Bitmap,
-                    imgThresholded.Bitmap);
-
-                bitmaps.ImageSet = sw.ElapsedMilliseconds;
-                bitmaps.FPS = bitmaps.Calculations + bitmaps.ImageSet;
-            }
+            analyzer.Analyze(bitmap, currentConfig);
         }
 
         private void Original_MouseDown(object sender, MouseButtonEventArgs e)
