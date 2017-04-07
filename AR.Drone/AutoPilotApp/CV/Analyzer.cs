d3a829ee66ac6551fae0e64895b889a009140083
Juan Manuel Servera <jmservera@outlook.com>
2017-04-08 00:34:08 +0200

    follow the orange

--- AR.Drone/AutoPilotApp/CV/Analyzer.cs
+++ AR.Drone/AutoPilotApp/CV/Analyzer.cs
@@ -2,8 +2,10 @@
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.Util;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,27 +22,34 @@ namespace AutoPilotApp.CV
 
         public void Analyze(System.Drawing.Bitmap bitmap, ColorConfig currentConfig)
         {
+            bool useMorphologic=true;
+            bool detectBox = false;
             System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
             bitmaps.UpdateImages(bitmap);
             using (Image<Bgr, byte> img = new Image<Bgr, byte>(bitmaps.Bitmap))
             {
                 UMat uimage = new UMat();
-                CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Hsv);
+                CvInvoke.CvtColor(img.Clone(), uimage, ColorConversion.Bgr2Hsv,3);
 
                 double cannyThreshold = 180.0;
-                double cannyThresholdLinking = 120.0;
+                double cannyThresholdLinking = 10.0;
                 UMat cannyEdges = new UMat();
-                CvInvoke.Canny(img, cannyEdges, cannyThreshold, cannyThresholdLinking);
-                LineSegment2D[] lines = CvInvoke.HoughLinesP(
-                   cannyEdges,
-                   1, //Distance resolution in pixel-related units
-                   Math.PI / 45.0, //Angle resolution measured in radians.
-                   20, //threshold
-                   30, //min Line width
-                   10); //gap between lines
+                CvInvoke.Canny(img.Clone(), cannyEdges, cannyThreshold, cannyThresholdLinking);
 
-                foreach (LineSegment2D line in lines)
-                    img.Draw(line, new Bgr(System.Drawing.Color.Green), 2);
+                if (detectBox)
+                {
+                    var boxList = BoxDetection(cannyEdges);
+                    foreach (var box in boxList)
+                    {
+                        for (int i = 0; i < box.Length; i++)
+                        {
+                            var pointA = box[i];
+                            var pointB = box[(i + 1) % box.Length];
+                            CvInvoke.Line(img, pointA, pointB, new Bgr(Color.DarkOrange).MCvScalar, 2);
+                        }
+                        //CvInvoke.Polylines(img, box, true, new Bgr(Color.DarkOrange).MCvScalar, 2);
+                    }
+                }
 
                 //use image pyr to remove noise
                 UMat pyrDown = new UMat();
@@ -52,6 +61,11 @@ namespace AutoPilotApp.CV
                 MCvScalar upper = new MCvScalar(currentConfig.HighH, currentConfig.HighS, currentConfig.HighV);
 
                 CvInvoke.InRange(uimage, new ScalarArray(lower), new ScalarArray(upper), imgThresholded);
+
+                if (useMorphologic)
+                {
+                    morphOps(imgThresholded);
+                }
 
                 //Calculate the moments of the thresholded image
                 var oMoments = CvInvoke.Moments(imgThresholded);
@@ -67,18 +81,85 @@ namespace AutoPilotApp.CV
                     int posX = (int)(dM10 / dArea);
                     int posY = (int)(dM01 / dArea);
                     CvInvoke.Circle(img, new System.Drawing.Point(posX, posY), (int)(dArea / 100000d),
-                        new MCvScalar(255, 0, 0), -1);
+                        new Bgr(Color.DarkOrange).MCvScalar, 4);
                 }
+
 
                 bitmaps.Calculations = sw.ElapsedMilliseconds;
                 sw.Restart();
                 bitmaps.UpdateImages(null, img.ToBitmap(),
-                    cannyEdges?.Bitmap,
-                    imgThresholded?.Bitmap);
+                    cannyEdges.Bitmap,
+                    imgThresholded.Bitmap);
 
                 bitmaps.ImageSet = sw.ElapsedMilliseconds;
                 bitmaps.FPS = bitmaps.Calculations + bitmaps.ImageSet;
             }
+        }
+
+        private void morphOps(UMat imgThresholded)
+        {
+
+            Mat erodeElement = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+            Mat dilateElement = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(8, 8), new Point(-1, -1));
+
+            CvInvoke.Erode(imgThresholded, imgThresholded, erodeElement, new Point(-1, -1), 2, BorderType.Default, CvInvoke.MorphologyDefaultBorderValue);
+            CvInvoke.Dilate(imgThresholded, imgThresholded, dilateElement, new Point(-1, -1), 2, BorderType.Default, CvInvoke.MorphologyDefaultBorderValue);
+        }
+
+        private List<Point[]> BoxDetection(UMat cannyEdges)
+        {
+            CvInvoke.Dilate(cannyEdges, cannyEdges, null,new Point(-1,-1), 1, BorderType.Constant, CvInvoke.MorphologyDefaultBorderValue);
+
+            List<Point[]> boxList = new List<Point[]>(); //a box is a rotated rectangle
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges.Clone(), contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size ==4) //The contour has 4 vertices.
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 40 || angle > 130)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(approxContour.ToArray());// CvInvoke.MinAreaRect(approxContour));
+
+                            }
+                        }
+                    }
+                }
+            }
+            return boxList;
         }
     }
 }
